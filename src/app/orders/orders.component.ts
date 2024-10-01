af2388a8543cf905c7c9c9fb43d19dcf2574b0f6
karnatipavan2580 <91123302+karnatipavan2580@users.noreply.github.com>
2024-10-02 02:20:07 +0530

    update more on orders placement

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -16,15 +16,52 @@ export class OrdersComponent {
   public orders: any = [];
   public layout: 'grid' | 'list' = 'list';
   public events: string[] = [];
-  constructor(private orderService: OrdersService) {
-    this.getOrders();
+  constructor(public orderService: OrdersService) {
+    // this.getOrders();
     this.events = [
       "Placed", "Accepted", "Cleaned, Checked & Packed", "Ready for delivery", "Delivered", "Time to Return", "Returned", "Checked & Added Back to Inventory"
     ];
+    this.selectTab('placed');
+  }
+
+  public selectTab(tab: string) {
+    console.log('tab => ', tab);
+    this.orderService.selectedTab = tab;
+    this.getOrders();
+    // switch (tab) {
+    //   case 'placed':
+    //     this.getOrders('Placed');
+    //     break;
+    //   case 'accepted':
+    //     this.orders = this.orders.filter((order: any) => order.Status === 'Accepted');
+    //     break;
+    //   case 'cleaned':
+    //     this.orders = this.orders.filter((order: any) => order.Status === 'Cleaned, Checked & Packed');
+    //     break;
+    //   case 'ready':
+    //     this.orders = this.orders.filter((order: any) => order.Status === 'Ready for delivery');
+    //     break;
+    //   case 'delivered':
+    //     this.orders = this.orders.filter((order: any) => order.Status === 'Delivered');
+    //     break;
+    //   case 'return':
+    //     this.orders = this.orders.filter((order: any) => order.Status === 'Time to Return');
+    //     break;
+    //   case 'returned':
+    //     this.orders = this.orders.filter((order: any) => order.Status === 'Returned');
+    //     break;
+    //   case 'checked':
+    //     this.orders = this.orders.filter((order: any) => order.Status === 'Checked & Added Back to Inventory');
+    //     break;
+    //   default:
+    //     this.orders = this.orders;
+    //     break;
+    // }
+
   }
 
   private getOrders(): void {
-    this.orderService.getAllOrders().subscribe((orders) => {
+    this.orderService.getOrders().subscribe((orders) => {
       console.log('orders => ', orders);
       this.orders = orders.map((t: any) => {
         t.expanded = false;
@@ -35,7 +72,7 @@ export class OrdersComponent {
 
   public setStatus(orderId: string) {
     this.orderService.getOrderState(orderId).subscribe(() => {
-      this.getOrders();
+      this.selectTab(this.orderService.selectedTab);
     });
   }
 }
